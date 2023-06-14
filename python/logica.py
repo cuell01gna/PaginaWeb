633db629d00fea282ae9a21f97242f8a54ebb7d2
Cuello Ignacio <cuelloignacio0@gmail.com>
2023-06-14 14:09:30 -0300

    copia pagina web prueba y prueba de python

---
+++ python/logica.py
@@ -0,0 +1,16 @@
+# Solicitar el primer número al usuario
+numero1 = float(input("Ingrese el primer número: "))
+
+# Solicitar el segundo número al usuario
+numero2 = float(input("Ingrese el segundo número: "))
+
+# Calcular la suma de los dos números
+suma = numero1 + numero2
+
+# Calcular el producto de los dos números
+producto = numero1 * numero2
+
+# Mostrar el resultado de la suma y el producto
+print("La suma de", numero1, "y", numero2, "es:", suma)
+print("El producto de", numero1, "y", numero2, "es:", producto)
+
